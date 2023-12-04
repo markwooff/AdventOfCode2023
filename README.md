45a618d19990afa4e4507591ff76e910602015f7
markwooff <mark.wooff@cse-cst.gc.ca>
2023-12-03 22:51:29 -0500

    Day 3 solution

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This repo contains solutions for the daily Advent of Code 2023 challenges.
 
 These solutions will be organized by day and will be attempted to be resolved in Javascript :sunglasses:
+
+**Edit:** I'm including some Python code into my arsenal as it makes it much easier to iterate through 2D arrays.
---
+++ day3/main.py
@@ -0,0 +1,42 @@
+import itertools, math, re, os
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_directory, 'input.txt')
+
+with open(file_path, 'r') as file:
+    inputArray = file.read().split("\n")
+
+box = list(itertools.product((-1, 0, 1), (-1, 0, 1)))
+parts_by_symbol = {
+    (i, j): (x, [])
+    for i, l in enumerate(inputArray)
+    for j, x in enumerate(l)
+    if x != "." and not x.isdigit()
+}
+
+part_sum = 0
+
+for i, l in enumerate(inputArray):
+    for match in re.finditer(r"\d+", l):
+        n = int(match.group(0))
+        boundary = {
+            (i + di, j + dj)
+            for di, dj in box
+            for j in range(match.start(), match.end())
+        }
+        if parts_by_symbol.keys() & boundary:
+            part_sum += n
+        for symbol in parts_by_symbol.keys() & boundary:
+            parts_by_symbol[symbol][1].append(n)
+
+## Part 1
+print("Solution for Part 1 is: %s" % part_sum)
+
+## Part 2
+print("Solution for Part 2 is: %s" % 
+    sum(
+        math.prod(parts)
+        for symbol, parts in parts_by_symbol.values()
+        if len(parts) == 2 and symbol == "*"
+    )
+)
